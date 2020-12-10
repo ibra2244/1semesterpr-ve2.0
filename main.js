af0cdec83e76c20d262f2465de48216cde4e6e3b
ibra2244 <74540980+ibra2244@users.noreply.github.com>
2020-12-10 16:37:25 +0100

    kontakt side pt1

--- main.js
+++ main.js
@@ -30,29 +30,29 @@ function toggleMenu() {
 }
 
 var typed = new Typed(".typing", {
-    strings: ["UI/UX Designer", "Programmør", "Freelancer", "Webudvikler", "Multimediedesigner"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-  });
-  
-  $('.carousel').owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplayTimeOut: 2000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: false
-      },
-      600: {
-        items: 2,
-        nav: false
-      },
-      1000: {
-        items: 3,
-        nav: false
-      }
+  strings: ["UI/UX Designer", "Programmør", "Freelancer", "Webudvikler", "Multimediedesigner"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
+
+$('.carousel').owlCarousel({
+  margin: 20,
+  loop: true,
+  autoplayTimeOut: 2000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false
+    },
+    600: {
+      items: 2,
+      nav: false
+    },
+    1000: {
+      items: 3,
+      nav: false
     }
-  });
+  }
+});
