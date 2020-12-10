f149c089f893786914c5cad771456ff8c219e350
ibra2244 <74540980+ibra2244@users.noreply.github.com>
2020-12-10 21:39:26 +0100

    java rettelse

--- main.js
+++ main.js
@@ -35,24 +35,3 @@ var typed = new Typed(".typing", {
   backSpeed: 60,
   loop: true
 });
-
-$('.carousel').owlCarousel({
-  margin: 20,
-  loop: true,
-  autoplayTimeOut: 2000,
-  autoplayHoverPause: true,
-  responsive: {
-    0: {
-      items: 1,
-      nav: false
-    },
-    600: {
-      items: 2,
-      nav: false
-    },
-    1000: {
-      items: 3,
-      nav: false
-    }
-  }
-});
